7e9fb9f5ba18c08e13bad1adcb0036012f9db589
n-elmasry <almassrynada411@gmail.com>
2024-04-27 11:44:12 +0500

    task 7 update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,30 @@
-# install Nginx
+# Install Nginx
 package { 'nginx':
     ensure => installed,
-
 }
 
-file { '/var/www/html/index.html':
-    content => 'Hello World!',
+# Ensure the directory exists
+file { '/etc/nginx/sites-available':
+    ensure => directory,
 }
 
+# Create or manage the content of the default file
+file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => template('nginx/default.erb'),
+}
+
+# Manage the redirect line in the default file
 file_line { 'redirect_me':
-    ensure => present,
-    path => '/etc/nginx/sites-available/default',
-    line => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    require => Package['nginx'],
-
+    ensure  => present,
+    path    => '/etc/nginx/sites-available/default',
+    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    require => File['/etc/nginx/sites-available/default'],
 }
+
+# Ensure Nginx service is running and enabled
 service { 'nginx':
-    ensure => running,
-    enable => true,
+    ensure  => running,
+    enable  => true,
     require => Package['nginx'],
 }
