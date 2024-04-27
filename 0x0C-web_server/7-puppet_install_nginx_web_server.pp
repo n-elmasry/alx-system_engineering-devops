5583b849db814af95b2c1d4bf98021924e54b791
n-elmasry <almassrynada411@gmail.com>
2024-04-27 11:52:41 +0500

    task 7 2nd update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,24 @@
-# Install Nginx
+# Install Nginx package
 package { 'nginx':
-    ensure => installed,
+  ensure => installed,
 }
 
-# Ensure the directory exists
-file { '/etc/nginx/sites-available':
-    ensure => directory,
+# Update the default HTML file
+file { '/var/www/html/index.nginx-debian.html':
+  ensure  => present,
+  content => 'Hello World!',
 }
 
-# Create or manage the content of the default file
-file { '/etc/nginx/sites-available/default':
-    ensure  => present,
-    content => template('nginx/default.erb'),
-}
-
-# Manage the redirect line in the default file
+# Add the rewrite rule to the default Nginx configuration
 file_line { 'redirect_me':
-    ensure  => present,
-    path    => '/etc/nginx/sites-available/default',
-    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-    require => File['/etc/nginx/sites-available/default'],
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  notify => Service['nginx'],
 }
 
 # Ensure Nginx service is running and enabled
 service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
+  ensure  => running,
+  enable  => true,
 }
