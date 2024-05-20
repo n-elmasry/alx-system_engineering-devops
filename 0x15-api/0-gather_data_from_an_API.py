d43a10d0cb216daf023e9547d6547ccd5f919328
n-elmasry <almassrynada411@gmail.com>
2024-05-20 20:57:15 +0500

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """returns information about a given employee todo list progress."""
 
-from sys import argv
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
