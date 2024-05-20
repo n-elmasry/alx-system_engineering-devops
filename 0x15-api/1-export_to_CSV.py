cbea333f27158d517221502b77ea9320aad4bc38
n-elmasry <almassrynada411@gmail.com>
2024-05-21 00:45:49 +0500

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """returns information about a given employee todo list progress."""
-
+import csv
 import requests
 from sys import argv
 
@@ -12,21 +12,21 @@ if __name__ == "__main__":
     # Fetch employee details
     user_url = f'https://jsonplaceholder.typicode.com/users/{id}'
     employee = requests.get(user_url).json()
-    employee_name = employee.get('name')
+    employee_name = employee.get('username')
 
     # Fetch employee todo list
     todos_url = f'https://jsonplaceholder.typicode.com/todos?userId={id}'
     all_tasks = requests.get(todos_url).json()
 
-    # Process tasks
-    done_tasks = [task.get('title')
-                  for task in all_tasks if task.get('completed')]
+    csv_rows = []
 
-    print(
-        f'Employee {employee_name} is done with tasks({len(done_tasks)}/'
-        f'{len(all_tasks)}):'
-    )
+    for task in all_tasks:
+        task_title = task.get('title')
+        task_completed = task.get('completed')
+        csv_rows.append([id, employee_name, task_completed, task_title])
 
-    # Print each completed task
-    for task in done_tasks:
-        print(f'\t {task}')
+    filename = f'{id}.csv'
+
+    with open(filename, 'w', newline='') as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        csv_writer.writerows(csv_rows)
