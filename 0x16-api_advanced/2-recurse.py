696e8b1c26cdd4b6f9e88d696aeafa8647af8e2c
n-elmasry <almassrynada411@gmail.com>
2024-06-05 00:10:10 +0500

    task2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Querying the Reddit API recursively
+returns a list containing the titles of all hot articles for a given subreddit.
 """
 import requests
 
@@ -9,17 +9,17 @@ def recurse(subreddit, hot_list=[], after=None):
     if type(subreddit) is not str:
         return None
     sub = subreddit
-    api_url = "https://api.reddit.com/r/{}/hot?after={}".format(sub, after)
-    headers = {'user-agent': 'safari:holberton/0.1.0'}
-    response = requests.get(api_url, headers=headers)
+    url = "https://api.reddit.com/r/{}/hot?after={}".format(sub, after)
+    headers = {"user-agent": "user"}
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
-        hot_posts = response.json()["data"]["children"]
+        posts = response.json()["data"]["children"]
         after = response.json()["data"]["after"]
         if after is None:
-            hot_list = titles(hot_posts, len(hot_posts))
+            hot_list = titles(posts, len(posts))
             return hot_list
         hot_list.append(recurse(subreddit, hot_list, after=after))
-        hot_list = titles(hot_posts, len(hot_posts))
+        hot_list = titles(posts, len(posts))
     else:
         return None
     return hot_list
