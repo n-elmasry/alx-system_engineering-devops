9df1d33b6633517d8f17c5e758bef663ab416c79
n-elmasry <almassrynada411@gmail.com>
2024-06-04 21:07:09 +0500

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
-Queries the Reddit API and prints the first ten hot posts for a given subreddit
+defines function to query the Reddit API for
+prints titles of the first 10 hot posts
 """
-import requests
 
 
 def top_ten(subreddit):
-    posts = ""
-    if (type(subreddit) is not str):
-        posts = "None\n"
-    else:
-        api_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        headers = {'user-agent': 'safari:holberton/0.1.0'}
-        response = requests.get(api_url, headers=headers)
-        if response.status_code is not 200:
-            posts = "None\n"
-        else:
-            posts_json = response.json().get("data").get("children")
-            for i in range(10):
-                post_title = posts_json[i].get("data").get("title")
-                posts += "{}\n".format(post_title)
-    print(posts, end="")
+    """
+    queries the Reddit API
+    prints the titles of the first 10 hot posts for subreddit
+    """
+    import json
+    import requests
+    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(
+        subreddit)
+    subreddit_info = requests.get(subreddit_URL,
+                                  headers={"user-agent": "user"},
+                                  allow_redirects=False).json()
+    if "data" not in subreddit_info:
+        print("None")
+        return
+    children = subreddit_info.get("data").get("children")
+    for child in children:
+        print(child.get("data").get("title"))
