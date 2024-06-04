e6efc13b5c48caa6d68ab33ca3194ef3138abb3c
n-elmasry <almassrynada411@gmail.com>
2024-06-04 21:21:36 +0500

    task1

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,28 @@
 #!/usr/bin/python3
 """
-defines recursive function to query the Reddit API
-for all hot articles of a given subreddit
+returns a list containing the titles of all hot articles for a given subreddit
 """
+import json
+import requests
 
 
 def recurse(subreddit, hot_list=[], after=None, count=0):
-    """
-    queries the Reddit API
-    returns list of titles of hot posts for subreddit
-    """
-    import json
-    import requests
+    """ returns a list containing the titles of all hot articles"""
     if after is None:
-        sub_URL = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+        url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     else:
-        sub_URL = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
+        url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
             subreddit, after)
-    subreddit_info = requests.get(sub_URL,
-                                  headers={"user-agent": "user"},
-                                  allow_redirects=False).json()
-    if "data" not in subreddit_info and hot_list == []:
+    info = requests.get(url,
+                        headers={"user-agent": "user"},
+                        allow_redirects=False).json()
+    if "data" not in info and hot_list == []:
         return None
-    children = subreddit_info.get("data").get("children")
-    for child in children:
-        hot_list.append(child.get("data").get("title"))
+    posts = info.get("data").get("children")
+    for post in posts:
+        hot_list.append(post.get("data").get("title"))
         count += 1
-    after = subreddit_info.get("data").get("after")
+    after = info.get("data").get("after")
     if after is None:
         return hot_list
     return (recurse(subreddit, hot_list, after, count))
